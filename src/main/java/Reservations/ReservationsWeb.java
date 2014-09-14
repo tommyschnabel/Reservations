fae072b6b8b420c715dc1fccf0f844676722001c
tommyschnabel <tommyschnabel@gmail.com>
2014-09-14 16:54:54 -0400

    Still trying to get Jersey to work

--- src/main/java/Reservations/ReservationsWeb.java
+++ src/main/java/Reservations/ReservationsWeb.java
@@ -1,10 +1,12 @@
 package main.java.reservations;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("reservations/")
 public class ReservationsWeb {
 
+	@GET
 	@Path("test/")
 	public String test() {
 		return "it works!";
