7bc54299ae33d518d8f0fe09dc886ae295471293
tommyschnabel <tommyschnabel@gmail.com>
2014-09-20 12:56:27 -0400

    Fixed my commit that erased everything

--- CleanAndBuildEclipse.bat
+++ CleanAndBuildEclipse.bat
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 gradle-2.0/bin/gradle.bat cleanEclipse
 gradle-2.0/bin/gradle.bat eclipse
 gradle-2.0/bin/gradle.bat build
+=======
+call gradle-2.0/bin/gradle.bat cleanEclipse
+call gradle-2.0/bin/gradle.bat eclipse
+call gradle-2.0/bin/gradle.bat build
+>>>>>>> Fixed my commit that erased everything
