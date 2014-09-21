a1f70f7ecfb48f83fa3cf1978052ee88f31b61c9
kkingery <kkingery@spsu.edu>
2014-09-21 12:38:04 -0400

    Added Database folder containing SQLite, SQL scripts, and database file

---
+++ Database/droptables.sql
@@ -0,0 +1,16 @@
+
+.print Tables before drop
+.tables
+
+drop table AirlineAdmin;
+drop table Mileage;
+drop table Customer;
+drop table Service;
+drop table MarketingAdmin;
+drop table Reservation;
+drop table Flight;
+drop table Price;
+drop table Airline;
+
+.print Tables after drop
+.tables
