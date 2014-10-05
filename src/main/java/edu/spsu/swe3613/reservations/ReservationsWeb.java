6bf0993f2025d9d0a57070cd1a4958435e8668d9
Alex-Honeycutt <alexodst141@gmail.com>
2014-10-05 14:57:37 -0400

    Search by price and by date added

--- src/main/java/edu/spsu/swe3613/reservations/ReservationsWeb.java
+++ src/main/java/edu/spsu/swe3613/reservations/ReservationsWeb.java
@@ -2,13 +2,17 @@ package edu.spsu.swe3613.reservations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.GregorianCalendar;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import edu.spsu.swe3613.examples.Search;
+
 
 @Path("reservations/")
 public class ReservationsWeb {
+	Search search_price;
 
 	@GET
 	@Path("test/")
@@ -23,9 +27,31 @@ public class ReservationsWeb {
 	
 	@GET
 	@Path("search/")
-	public List<Flight> search() {
+	public List<Flight> search(float p) {
 		//Start with one search param then move on!!!
-		return new ArrayList<Flight>();
+		//Start with all flights that are less than or equal to given price
+		//ArrayList<Flight> All_Flights = new ArrayList<Flight>();
+		//ArrayList<Flight> search_Flights = new ArrayList<Flight>();
+		//@Inject
+		//All_Flights = public List<Flight> getAllFlights()
+		
+		
+		return search_price.Search_By_Price(p);
+		
+		
+		
+		
+	}
+	
+	@GET
+	@Path("search/")
+	public List<Flight> search(String d) {
+		//Start with one search param then move on!!!
+		//ArrayList<Flight> All_Flights = new ArrayList<Flight>();
+		
+		
+		
+		return search_price.Search_By_Date(d);
 	}
 	
 }
