e5f0382d06556fc20e3cd99a9e89d0fded4605a1
yuyang10 <yuyang9210@gmail.com>
2014-10-15 13:42:51 -0400

    Fixed css for webapp

--- src/main/webapp/controllers.js
+++ src/main/webapp/controllers.js
@@ -3,9 +3,17 @@ var controllers = angular.module('reservationsControllers', []);
 //HEADER CONTROLLER
 controllers.controller('headerController', ['$scope', '$location', '$http',
   		function ($scope, $location, $http) {
-            $scope.register = function() {
-
-            };
+		    $scope.changePage = function(path) {
+		        $location.path(path);
+		    };
+			
+		    $scope.testJackson = function() {
+                $http({ 
+                    url: 'http://localhost:8080/reservations/api/login/', 
+                    method: 'POST', 
+                    data: { email: 'fake@fakemail.org', password: 'password'}
+                });
+		    }
   		}
 	]
 );
@@ -27,25 +35,11 @@ controllers.controller('loginController', ['$scope', '$location',
 );
 
 
-//REGISTER CONTROLLER
+//TEST CONTROLLER
 controllers.controller('registerController', ['$scope', '$location',
   		function ($scope, $location) {
   		}
 	]
-);
-
-//FLIGHT CONTROLLER
-controllers.controller('flightController', ['$scope', '$location',
-        function ($scope, $location) {
-        }
-    ]
-);
-
-//PASSENGER INFORMATION CONTROLLER
-controllers.controller('passengerController', ['$scope', '$location',
-        function ($scope, $location) {
-        }
-    ]
 );
 
 //TEST CONTROLLER
@@ -55,8 +49,7 @@ controllers.controller('testController', ['$scope', '$http',
 		    $scope.errorMessages = 'None';
 			
 		    $scope.testJackson = function() {
-				$http({ 
-                    url: 'http://localhost:8080/reservations/api/login/',
+				$http({ url: 'http://localhost:8080/reservations/api/login/',
 					method: 'POST', 
 					data: { 
 						email: 'fake@fakemail.org', 
