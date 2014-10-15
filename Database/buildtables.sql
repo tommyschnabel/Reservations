c7c36a7014e8caf83057c6f679d4e0f19e9adfe8
kkingery <kkingery@spsu.edu>
2014-10-15 15:01:31 -0400

    More tweaking the DB file and script

--- Database/buildtables.sql
+++ Database/buildtables.sql
@@ -65,7 +65,7 @@ create table Customer(
 	Password	text
 	);
 insert into Customer values
-	('admin','John','Doe','johndoe@gmail.com','root');
+	(1,'John','Doe','johndoe@gmail.com','root');
 -- Airline Admin
 drop table if exists AirlineAdmin;
 create table AirlineAdmin(
@@ -113,7 +113,6 @@ create table Reservation(
 	ID		integer primary key,
 	CustomerID	integer,
 	FlightID	integer,
-	SeatQuantity	integer,
 	FlightClass	text,
 	foreign key (CustomerID) references Customer(ID)
 		on delete cascade on update cascade,
